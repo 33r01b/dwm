311528f275f7c2c785c5acbee11a9b97605a7a63
33r01b <33r01b@gmail.com>
2022-01-03 20:28:07 +0300

    update border colors

--- config.h
+++ config.h
@@ -7,16 +7,17 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Noto Mono:size=9", "TerminessTTF Nerd Font Mono:size=9" };
 static const char dmenufont[]       = "Noto Mono:size=9";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_active[]      = "#333333";
+static const char col_active[]      = "#222222";
+static const char col_active_border[] = "#333333";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_active,  col_active  },
+	[SchemeSel]  = { col_gray4, col_active,  col_active_border  },
 };
 
 /* tagging */
